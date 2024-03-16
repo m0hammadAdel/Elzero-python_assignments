435d403ae5155e4d6d8e53bb3c4f97c1e078c32d
Mohammad Adel Abdelaleam <mohammadadel8727@gmail.com>
2024-03-16 17:57:21 +0200

    added first week assignments

---
+++ 1-week_assignments/README.md
@@ -0,0 +1,17 @@
+# Week 1 Assignments
+This week was an introduction to what is coming in this course with a little explnation about variables and data types
+
+## First Assignment
+is to make a multiple line comment
+
+## Second Assignment
+is to create three vriables name, age and country with string type
+
+## third Assignemnt
+is to print each variable in a new line with an explantion to it
+
+## fourth Assignment
+is to concatinate all the three variables in a sentence
+
+## fifth Assignment
+is to print the type of each variable in a new line
---
+++ 1-week_assignments/assignments.py
@@ -0,0 +1,28 @@
+# ----------- first assignment -----------
+# in python there is not actually a way
+# to make multiple line comments
+'''
+but we come around some ways to
+do it like this
+'''
+
+# ----------- second assignment -----------
+name = "Mohammad"
+age = "22"
+country = "Egypt"
+
+# ----------- third assignment -----------
+print("Name: " + name)
+print("Age: " + age)
+print("Country: " + country)
+
+# ----------- fourth assignment -----------
+print(
+    "Hello, My Name Is " + name + " And Iam "+ age + " Years Old and I Live in " + country + " ."
+)
+# I know it might look weird but the next week videos will make it better
+
+# ----------- fifth assignment -----------
+print(type(name))
+print(type(age))
+print(type(country))
